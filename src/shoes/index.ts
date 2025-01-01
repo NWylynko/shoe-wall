5f287654d559bb8e4e430aa8e3748eed3e9f9081
Nick Wylynko <nick@wylynko.com>
2025-01-02 02:51:58 +0800

    this very loosely works

---
+++ src/shoes/index.ts
@@ -0,0 +1,81 @@
+import shoe1 from "./Adidas Campus 00s Core Black.avif"
+import shoe2 from "./Adidas Samba OG Cloud White Core Black.avif"
+import shoe3 from "./Adidas Yeezy Slide Slate Marine.avif"
+import shoe4 from "./Air Jordan 1 Retro High Chicago.avif"
+import shoe5 from "./Air Jordan 1 Retro Low Medium Olive.avif"
+import shoe6 from "./Air Jordan 1 Retro Low Mocha.avif"
+import shoe7 from "./Air Jordan 1 Retro Low Travis Scott Velvet Brown.avif"
+import shoe8 from "./Air Jordan 3 Retro Black Cement.avif"
+import shoe9 from "./Air Jordan 4 Retro Bred.avif"
+import shoe10 from "./Air Jordan 4 Retro Fear 2024.avif"
+import shoe11 from "./Air Jordan 4 Retro Military Blue 2024.avif"
+import shoe12 from "./Air Jordan 4 Retro Oxidized Green.avif"
+import shoe13 from "./Air Jordan 4 Retro Red Cement.avif"
+import shoe14 from "./Air Jordan 4 Retro SE Paris.avif"
+import shoe15 from "./Air Jordan 4 Retro White Thunder.avif"
+import shoe16 from "./Air Jordan 11 Retro Legend Blue 2024.avif"
+import shoe17 from "./Air Jordan Cut The Check Travis Scott Mocha.avif"
+import shoe18 from "./ASICS Gel 1130 Black Silver.avif"
+import shoe19 from "./ASICS Gel 1130 White Silver.avif"
+import shoe20 from "./ASICS Gel Kayano 14 Cream Black Plum.avif"
+import shoe21 from "./Bape Sta 4 Silver.avif"
+import shoe22 from "./Crocs Classic Clog Lightning McQueen.avif"
+import shoe23 from "./Hoka Clifton 9 Harbor Mist.avif"
+import shoe24 from "./New Balance 9060 Black Castlerock Grey.avif"
+import shoe25 from "./Nike Air Force 1 Low White.avif"
+import shoe26 from "./Nike Dunk High Wu Tang 2024.avif"
+import shoe27 from "./Nike Dunk Low Grey Fog.avif"
+import shoe28 from "./Nike Dunk Low Retro White Black 2021.avif"
+import shoe29 from "./Nike Hot Step Drake NOCTA Black.avif"
+import shoe30 from "./Nike Kobe 5 Protro Ray.avif"
+import shoe31 from "./Nike SB Dunk Low Wizard of Oz.avif"
+import shoe32 from "./Timberland 6 Inch Premium Waterproof Wheat.avif"
+import shoe33 from "./UGG Tasman Slipper Black.avif"
+import shoe34 from "./Yeezy Boost 350 V2 Onyx.avif"
+import shoe35 from "./Yeezy Boost 350 V2 Pure Oat.avif"
+import shoe36 from "./Yeezy Boost 350 V2 Steel Grey.avif"
+import shoe37 from "./Yeezy Boost 700 Metallic.avif"
+import shoe38 from "./Yeezy Foam RNNR Onyx.avif"
+import shoe39 from "./Yeezy Slide Dark Onyx.avif"
+
+export const shoes = [
+  shoe1,
+  shoe2,
+  shoe3,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  shoe8,
+  shoe9,
+  shoe10,
+  shoe11,
+  shoe12,
+  shoe13,
+  shoe14,
+  shoe15,
+  shoe16,
+  shoe17,
+  shoe18,
+  shoe19,
+  shoe20,
+  shoe21,
+  shoe22,
+  shoe23,
+  shoe24,
+  shoe25,
+  shoe26,
+  shoe27,
+  shoe28,
+  shoe29,
+  shoe30,
+  shoe31,
+  shoe32,
+  shoe33,
+  shoe34,
+  shoe35,
+  shoe36,
+  shoe37,
+  shoe38,
+  shoe39,
+]
